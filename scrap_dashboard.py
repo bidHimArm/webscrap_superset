f0efd36379def008e5168c7fabb0661d99003a2b
bidhimarm <mehdi.brahimi@outlook.com>
2018-12-19 16:54:04 +0100

    impression réussie, commit avant mise sur Saagie

--- scrap_dashboard.py
+++ scrap_dashboard.py
@@ -1,18 +1,24 @@
 import codecs
-from progress.bar import Bar 
+from progress.bar import Bar
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 import time
+import datetime
+
 
 username = input('Enter username: ')
 pswd = input("Password:")
 dashboard=input("Input dashboard name:") # for exemple "Degrilleur - rapport"
-
-
 Url="https://3-1457-superset.public.a4.saagie.io/login/"
+
+
+# username = "admin"
+# pswd = "superset"
+# dashboard="Degrilleur - rapport"
+
 
 
 def superset_login(url,Username,Password,DashboardName):
@@ -20,8 +26,8 @@ def superset_login(url,Username,Password,DashboardName):
 	options.set_headless(True)
 	FFprofile = webdriver.FirefoxProfile()
 	FFprofile.set_preference('print.always_print_silent',True)
-	driver = webdriver.Firefox(FFprofile,options=options)
-
+	FFprofile.set_preference('print.printer', "Microsoft Print to PDF")
+	driver = webdriver.Firefox(firefox_profile=FFprofile, options=options)
 	driver.get(url)
 	driver.find_element_by_id("username").send_keys(Username)
 	driver.find_element_by_id("password").send_keys(Password)
@@ -32,15 +38,19 @@ def superset_login(url,Username,Password,DashboardName):
 	driver.find_element_by_link_text("Dashboards").click()
 	driver.find_element_by_link_text(DashboardName).click()
 	WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID,'GRID_ID')))
-	# driver.save_screenshot("rapport_journalier.png")
-	print('Start printing...')
-	driver.execute_script("window.print()")
-	bar = Bar('Processing', max=20)
-	for i in range(20):
-		time.sleep(0.9)
+	bar = Bar('Processing', max=3)
+	for i in range(3):
+		time.sleep(1)
 		bar.next()
 	bar.finish()
 	
+	driver.execute_script("(document.getElementsByClassName('navbar'))[0].style.display = 'none';")
+	driver.execute_script("(document.getElementsByClassName('dashboard-header'))[0].style.display = 'none';")
+	driver.get_screenshot_as_file('/tmp/google.png')
+	date_v  = datetime.datetime.today().strftime('%Y-%m-%d')
+	driver.save_screenshot("rapport_journalier "+ date_v +".png")
+	print('Start printing...')
+	driver.execute_script("window.print()")
 	print('The dashboard has been printed')
 	
 	driver.quit()
